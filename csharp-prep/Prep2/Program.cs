f1eddd6940c212fadc05160d72087e1e86b1e675
SiGuyHere <siiguyhere@gmail.com>
2024-09-23 13:06:14 -0600

    indentong

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -8,21 +8,27 @@ class Program
         string stringGrade = Console.ReadLine();
         int grade = int.Parse(stringGrade);
         string letterGrade = "X";
-        if (grade >= 90) {
+        if (grade >= 90) 
+        {
             letterGrade = "A";
-        } else if (grade >= 80) {
+        } else if (grade >= 80) 
+        {
             letterGrade = "B";
-        } else if (grade >= 70) {
+        } else if (grade >= 70) 
+        {
             letterGrade = "C";
-        } else if (grade >= 60) {
+        } else if (grade >= 60) 
+        {
             letterGrade = "D";
         } else {
             letterGrade = "F";
         }
         Console.WriteLine($"Your letter grade was {letterGrade}");
-        if (grade >= 70) {
+        if (grade >= 70) 
+        {
             Console.WriteLine("You passed");
-        } else {
+        } else 
+        {
             Console.WriteLine("Try harder next time.");
         }
     }
